c541aebb9d8948420178bda83d2fb95d5e532a69
DarriusB <darrius.bradford@gmail.com>
2022-05-11 08:04:26 -0700

    Exercise Completed.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{VGamesOrganize()}"); 
+        }
+
+        public static string VGamesOrganize()
+        {
+            var vGames = new List<string>() {
+                "Mortal Kombat",
+                "Tekken",
+                "Super Smash Bros",
+                "The Last of Us",
+                "Resident Evil",
+                "GTA 5",
+                "Need 4 Speed",
+                "Madden",
+                "NBA 2k"
+            };
+
+            foreach (var game in vGames)
+            {
+                var result = vGames.OrderByDescending(x => x.Length).ToList();
+                foreach (var game2 in result)
+                {
+                    Console.WriteLine(game2);
+                }
+                break;
+            }
+            return null;
+        }
+    }
+}
